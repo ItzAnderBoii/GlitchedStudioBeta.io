57b7ed28f51eb4c9a185068b11c7d1bcfea82071
AdoesCode <105987713+ItzAnderBoii@users.noreply.github.com>
2025-01-02 15:15:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let warnButton = document.getElementById('warn');
+
+warnButton.onclick = function() {
+    window.alert("The website is still in Beta, which means the website is not done yet but would be updated anytime")
+}
